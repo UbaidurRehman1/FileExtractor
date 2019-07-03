f94cb633177ca0fe654ab8866af686f6ff0d9188
UbaidurRehman1 <ubaidshan007@gmail.com>
2019-07-03 16:38:24 +0500

    documentation add

--- src/main/java/com/ubaid/app/App.java
+++ src/main/java/com/ubaid/app/App.java
@@ -16,7 +16,7 @@ public class App
 		App app = new App();
 		
 		
-		//archiver
+		//create archiver
 		Archiver archiver = app.getArchiver();
 
 		//get current path
@@ -24,18 +24,30 @@ public class App
 
 		//get current directory
 		File directory = new File(path);
+	
+		//get filter
 		FileFilter filter = app.new AcceptTGZFile();
+		
+		//get all files in the current directory
 		File[] files = directory.listFiles(filter);
+
+		//counters
 		int counter = 0;
 		int deletedFile = 0;
+		
+		//loop which delete each file in this directory
 		for(int i = 0; i < files.length; i++)
 		{
+			//get file
 			File archive = files[i];
 			System.out.println("\n===> Extracting File: " + archive.getName());
 
 			try
 			{
+				//extracting it in the current directory
 				archiver.extract(archive, directory);
+				
+				//if no exception occur then delete
 				if(archive.delete())
 				{
 					System.out.println("\n===> " + ++deletedFile  + "File Deleted: " + archive.getName());
@@ -44,22 +56,37 @@ public class App
 			}
 			catch (IOException e)
 			{
+				//on exception, simppy give the message
 				System.err.println("\n===> " + ++counter + ": Extraction Error ---- File Not Deleted " + archive.getName());
 			}
 			
 		}		
 	}
 	
+	/**
+	 * 
+	 * @return current direcotry where jar will place
+	 */
 	private String getCurrentDir()
 	{
 		return System.getProperty("user.dir");
 	}
 	
+	/**
+	 * 
+	 * @return Archiver which is used for extracting .tgz files
+	 */
 	private Archiver getArchiver()
 	{
 		return ArchiverFactory.createArchiver(ArchiveFormat.TAR, CompressionType.GZIP);
 	}
 	
+	/**
+	 * a private class which implements FileFilter 
+	 * and it accept only those files which ends with .tgz
+	 * @author UbaidurRehman
+	 *
+	 */
 	private class AcceptTGZFile implements FileFilter
 	{
 
